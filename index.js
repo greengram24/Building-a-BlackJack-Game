e9697e22f4eead9e09269235ab06dbe8151c9842
beans24 <pascuavhince24@gmail.com>
2026-03-15 22:42:42 +0800

    learned how to return values

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 // let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+function getRandomCard(){
+
+}
+
 
 function startGame(){
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i<cards.length; i++){
+        cardsEl.textContent +=cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: "+ sum
     if (sum <= 20){
-        message ="Do you want to draw a new card? 🎴"      
+        message ="Do you want to draw a new card?"      
     } else if (sum === 21){
-        message = "Nice one. You've got blackjack. ♠️"
+        message = "Nice one. You've got blackjack."
         hasBlackJack = true
     } else {
-        message = "You lose! 😓"
+        message = "You lose!"
         isAlive= false
     }
 
@@ -25,3 +41,14 @@ function startGame(){
 
     console.log(message)
 }
+
+function newCard(){
+    let card = 7
+    sum += card
+    cards.push(card)
+    renderGame()
+}
+
+
+// learned how to return values
+
