40f8f00f262144244ee6b75ba26c9e95e4707d8f
beans24 <pascuavhince24@gmail.com>
2026-03-16 08:28:34 +0800

    until lesson 52
    -random floor
    -&& and || Operators
    -Intro to objects

--- index.js
+++ index.js
@@ -1,54 +1,96 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-// let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-
-function getRandomCard(){
-
+let player = {
+    name: "Vhi",
+    chips: 145
 }
 
 
-function startGame(){
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
-function renderGame(){
+function renderGame() {
     cardsEl.textContent = "Cards: "
-    for (let i = 0; i<cards.length; i++){
-        cardsEl.textContent +=cards[i] + " "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
     }
-
-    sumEl.textContent = "Sum: "+ sum
-    if (sum <= 20){
-        message ="Do you want to draw a new card?"      
-    } else if (sum === 21){
-        message = "Nice one. You've got blackjack."
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You lose!"
-        isAlive= false
+        message = "You're out of the game!"
+        isAlive = false
     }
-
     messageEl.textContent = message
-
-    console.log(message)
-}
-
-function newCard(){
-    let card = 7
-    sum += card
-    cards.push(card)
-    renderGame()
 }
 
 
-// learned how to return values
+function newCard() {
+    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+    if (isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    } else{
+        return
+    }
+    
+}
 
+let airbnb = {
+    title: "Entire condo in Baguio, Philippines",
+    guests: 11,
+    bedroom: 3,
+    beds: 11,
+    baths: 3,
+    available: true,
+    offers: ["Wifi","Kitchen","Workspace","Parking"]
+}
+
+console.log(airbnb.offers)
+
+// 3/16/2026
+// Aside- Math.random()
+// Flooring the number with Math.floor()
+// Using Math.random() and Math.floor() to createa dice
+// Completing our dice function
+// Make getRandomCard() work
+// complete getRandomCard()
+// our new card feature is broken
+// && and || operators
+// objects sneak peek
+//Aside- intro to objects
+// Create your first object
